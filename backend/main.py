b689a60579a60cfbdceb02b79b45d44bc7959b31
aashnnasoni <aashnnasoni1@gmail.com>
2025-10-25 01:55:42 -0400

    Add Entrypoint in backend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+from auth import router as auth_router
 import os
 
 load_dotenv()
@@ -14,7 +15,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.include_router(auth_router)
 
 @app.get("/")
 async def root():
